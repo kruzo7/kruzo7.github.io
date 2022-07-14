cf1b2a1f77d1f52f5991fd143f311dc26bad2c8d
Waldek <kruzo7@gmail.com>
2022-07-14 22:11:10 +0200

    carousel

--- js/sitescript.js
+++ js/sitescript.js
@@ -1,4 +1,6 @@
-$(window).on("load", function() {
+$(window).on("load", function () {
 	// Animate loader off screen
 	$(".loader").fadeOut("slow");;
 });
+
+bootstrap.Carousel.Default.interval = 500;
